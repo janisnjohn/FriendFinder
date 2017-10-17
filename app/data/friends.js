33aa441fc0ebd8a69f9999b4b117c48ec3a47264
janisnjohn <janisnjohn@gmail.com>
2017-10-17 13:51:43 -0700

    added functionality to survey api

---
+++ app/data/friends.js
@@ -0,0 +1,32 @@
+// ===============================================================================
+// DATA
+// Below data will hold all of the reserved tables.
+// Initially we just set it equal to a "dummy" customer.
+// But you could have it be an empty array as well.
+// ===============================================================================
+
+var friendsArray = [
+  {
+    Name: "Ahmed",
+    photo: "https://pbs.twimg.com/profile_images/706512317165510657/ne8Uf5ot.jpg",
+    score: [5,1,4,4,5,1,2,5,4,1]
+  },
+  {
+    Name: "John",
+    photo: "https://grist.files.wordpress.com/2017/03/seanawatkins.jpg?w=640",
+    score: [3,3,4,4,5,3,5,5,4,1]
+  },
+  {
+    Name: "Mike",
+    photo: "http://techcircle.vccircle.com/wp-content/uploads/2015/10/Omid-Kordestani-300x300.jpg",
+    score: [4,3,4,4,5,3,1,5,4,1]
+  },
+  {
+    Name: "Steve",
+    photo: "https://qzprod.files.wordpress.com/2016/02/h_50472570.jpg?quality=80&strip=all",
+    score: [4,3,1,4,5,3,1,5,4,1]
+  }
+];
+
+// Note how we export the array. This makes it accessible to other files using require.
+module.exports = friendsArray;
