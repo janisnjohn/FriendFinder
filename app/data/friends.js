3332a0d2595aea58e2bdcb34278c69ae5e04201e
janisnjohn <janisnjohn@gmail.com>
2017-10-18 14:25:16 -0700

    with comparison progress

--- app/data/friends.js
+++ app/data/friends.js
@@ -7,24 +7,28 @@
 
 var friendsArray = [
   {
-    Name: "Ahmed",
+    name: "Ahmed",
     photo: "https://pbs.twimg.com/profile_images/706512317165510657/ne8Uf5ot.jpg",
-    score: [5,1,4,4,5,1,2,5,4,1]
+    score: [5,1,4,4,5,1,2,5,4,1],
+    total: 32
   },
   {
-    Name: "John",
+    name: "John",
     photo: "https://grist.files.wordpress.com/2017/03/seanawatkins.jpg?w=640",
-    score: [3,3,4,4,5,3,5,5,4,1]
+    score: [3,3,4,4,5,3,5,5,4,1],
+    total: 37
   },
   {
-    Name: "Mike",
+    name: "Mike",
     photo: "http://techcircle.vccircle.com/wp-content/uploads/2015/10/Omid-Kordestani-300x300.jpg",
-    score: [4,3,4,4,5,3,1,5,4,1]
+    score: [4,3,4,4,5,3,1,5,4,1],
+    total: 34
   },
   {
-    Name: "Steve",
+    name: "Steve",
     photo: "https://qzprod.files.wordpress.com/2016/02/h_50472570.jpg?quality=80&strip=all",
-    score: [4,3,1,4,5,3,1,5,4,1]
+    score: [4,3,1,4,5,3,1,5,4,1],
+    total: 31
   }
 ];
 
