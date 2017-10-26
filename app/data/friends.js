b7c44ffc185ce6bfe442b66783083f957cf5ad4e
janisnjohn <janisnjohn@gmail.com>
2017-10-26 15:30:14 -0700

    used array formula working

--- app/data/friends.js
+++ app/data/friends.js
@@ -9,8 +9,8 @@ var friendsArray = [
   {
     name: "Jason",
     photo: "https://images.contentful.com/0pnkpx3hfneg/1UTu5LX1YUWaoWKeqass8e/4cf9e1eebbf60a2359590b1ca8314c42/Luke-Downey.jpg",
-    score: [5,1,4,4,5,1,2,5,4,1],
-    total: 32
+    score: [4,4,4,4,4,4,4,4,4,4],
+    total: 40
   },
   {
     name: "John",
@@ -27,8 +27,8 @@ var friendsArray = [
   {
     name: "Steve",
     photo: "http://grinkecreative.com/wp-content/uploads/2014/04/DSF5867sm-300x300.jpg",
-    score: [4,3,1,4,5,3,1,5,4,1],
-    total: 31
+    score: [3,3,3,3,3,3,3,3,3,3],
+    total: 30
   }
 ];
 
